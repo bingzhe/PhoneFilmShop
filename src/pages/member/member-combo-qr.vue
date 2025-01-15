53faf7b8289325f73593b58e25a2ac137a22bf2c
bingzhe <874177815@qq.com>
2025-01-15 14:39:00 +0800

    fix(page): 生成的二维码遮挡弹窗

--- src/pages/member/member-combo-qr.vue
+++ src/pages/member/member-combo-qr.vue
@@ -23,7 +23,12 @@
       </view>
 
       <view class="flex items-center justify-center w-270px h-270px bg-#fff">
-        <canvas id="qrcode" canvas-id="qrcode" style="width: 250px; height: 250px"></canvas>
+        <canvas
+          id="qrcode"
+          canvas-id="qrcode"
+          type="2d"
+          style="width: 250px; height: 250px"
+        ></canvas>
       </view>
     </view>
   </view>
@@ -97,14 +102,48 @@ const generateQrCode = (val: string) => {
       qrCode = null
     }
 
+    // #ifdef MP-WEIXIN
     qrCode = new UQRCode()
 
     qrCode.data = val
     qrCode.size = 250
     qrCode.make()
-    const canvasContext = uni.createCanvasContext('qrcode', this) // 如果是组件，this必须传入
-    qrCode.canvasContext = canvasContext
-    qrCode.drawCanvas()
+
+    // const canvasContext = uni.createCanvasContext('qrcode', this) // 如果是组件，this必须传入
+    // qrCode.canvasContext = canvasContext
+    // qrCode.drawCanvas()
+
+    // wx.createSelectorQuery()
+    //   .select('#qrcode') // 在 WXML 中填入的 id
+    //   .node(({ node: canvas }) => {
+    //     const context = canvas.getContext('2d')
+    //     qrCode.canvasContext = context
+    //     qrCode.drawCanvas(context)
+    //   })
+    //   .exec()
+
+    wx.createSelectorQuery()
+      .select('#qrcode') // 在 WXML 中填入的 id
+      .fields({ node: true, size: true })
+      .exec((res) => {
+        // Canvas 对象
+        const canvas = res[0].node
+        // Canvas 画布的实际绘制宽高
+        const renderWidth = res[0].width
+        const renderHeight = res[0].height
+        // Canvas 绘制上下文
+        const context = canvas.getContext('2d')
+
+        // 初始化画布大小
+        const dpr = (wx as any).getWindowInfo().pixelRatio
+        canvas.width = renderWidth * dpr
+        canvas.height = renderHeight * dpr
+        context.scale(dpr, dpr)
+
+        qrCode.canvasContext = context
+        qrCode.drawCanvas(context)
+      })
+    // #endif
   } catch (error) {
     console.error(error)
   }
